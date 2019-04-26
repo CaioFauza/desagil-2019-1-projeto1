89823299e957c2a25548aeaefa7015066053af11
Caio Fauza <caiofauza@gmail.com>
2019-04-26 20:42:48 -0300

    Input de contato

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MorseTranslate.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MorseTranslate.java
@@ -1,13 +1,16 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 
+
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+
 
 public class MorseTranslate extends AppCompatActivity {
 
@@ -16,11 +19,23 @@ public class MorseTranslate extends AppCompatActivity {
     private String wordTranslated;
     public boolean word;
 
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_morse_translate);
         Translator translator = new Translator();
+        Intent morseTranslateIntent = getIntent();
+        String prevActivity = morseTranslateIntent.getStringExtra("morseTranslateActivity");
+        Intent contactActivityIntent = new Intent(this, ContatosActivity.class);
+
+
 
         Button buttonMorse = findViewById(R.id.buttonMorse);
         TextView morseText = findViewById(R.id.morseText);
@@ -28,6 +43,39 @@ public class MorseTranslate extends AppCompatActivity {
         Button butonDel = findViewById(R.id.buttonDel);
         Button buttonSpace = findViewById(R.id.buttonSpace);
         Button buttonBack = findViewById(R.id.buttonBack);
+        TextView nameText = findViewById(R.id.nameText);
+        TextView numberText = findViewById(R.id.numberText);
+        TextView newMessageText = findViewById(R.id.newMessageText);
+        Button finishButton = findViewById(R.id.buttonFinish);
+
+        if(prevActivity.equals("contactListName")){
+            nameText.setVisibility(View.VISIBLE);
+            finishButton.setOnClickListener((view) -> {
+                morseTranslateIntent.putExtra("morseTranslateActivity", "contactListNumber");
+                startActivity(morseTranslateIntent);
+
+        });
+
+        }
+        if(prevActivity.equals("contactListNumber")){
+            numberText.setVisibility(View.VISIBLE);
+            finishButton.setOnClickListener((view) -> {
+                startActivity(contactActivityIntent);
+
+            });
+
+        }
+        if(prevActivity.equals("newMessage")){
+            nameText.setVisibility(View.INVISIBLE);
+            numberText.setVisibility(View.INVISIBLE);
+
+            newMessageText.setVisibility(View.VISIBLE);
+
+
+
+        }
+
+
 
         buttonSet.setOnClickListener((view) -> {
 
@@ -73,7 +121,7 @@ public class MorseTranslate extends AppCompatActivity {
             word = false;
             preTranslated += ".";
             morseText.append(".");
-            });
+        });
 
 
         buttonMorse.setOnLongClickListener((view) -> {
